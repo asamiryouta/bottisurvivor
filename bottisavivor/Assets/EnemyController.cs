786022b4b57241b022273015e209c96e29c2a056
neco0621 <kane263shun@icloud.com>
2023-06-23 12:15:42 +0900

    up

--- bottisavivor/Assets/EnemyController.cs
+++ bottisavivor/Assets/EnemyController.cs
@@ -4,19 +4,50 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    private GameObject playerObject;    //プレイヤーオブジェクト
-    private Vector3 PlayerPosition;     //プレイヤーの位置情報
-    private Vector3 EnemyPosition;      //敵の位置情報
+    private Rigidbody2D rb;
+    private GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerObject = GameObject.FindWithTag("player");
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectsWithTag("Player")[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pv = player.transform.position;
+        Vector3 ev = transform.position;
+
+        float p_vX = pv.x - ev.x;
+        float p_vY = pv.y - ev.y;
+
+        float vx = 0f;
+        float vy = 0f;
+
+        float sp = 10f;
+
+        // 減算した結果がマイナスであればXは減算処理
+        if (p_vX < 0)
+        {
+            vx = -sp;
+        }
+        else
+        {
+            vx = sp;
+        }
+
+        // 減算した結果がマイナスであればYは減算処理
+        if (p_vY < 0)
+        {
+            vy = -sp;
+        }
+        else
+        {
+            vy = sp;
+        }
+
+        transform.Translate(vx / 50, vy / 50, 0);
     }
 }
