b958b8b249a0fa2d38b83389b84b208aa80d69f6
neco0621 <kane263shun@icloud.com>
2023-06-28 16:41:07 +0900

    up

---
+++ bottisavivor/Assets/Spawn.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    //プレハブ格納用
+    public GameObject EnemyPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.frameCount % 1000 == 0)
+        {
+            float x = Random.Range(-5.0f, 5.0f);
+            float y = Random.Range(-5.0f, 5.0f);
+            Vector3 pos = new Vector3(x, y, 0);
+            //プレハブを指定位置に生成
+            Instantiate(EnemyPrefab, pos , Quaternion.identity);
+        }
+    }
+}
