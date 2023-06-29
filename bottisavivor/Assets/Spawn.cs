ffa50b1d457b687521c0b9cec3eb5f3eb815b7ed
neco0621 <kane263shun@icloud.com>
2023-06-29 17:54:57 +0900

    up

--- bottisavivor/Assets/Spawn.cs
+++ bottisavivor/Assets/Spawn.cs
@@ -6,7 +6,11 @@ public class Spawn : MonoBehaviour
 {
     //プレハブ格納用
     public GameObject EnemyPrefab;
+    //　今何人の敵を出現させたか（総数）
+    private int numberOfEnemyPrefab;
 
+    //　この場所から出現する敵の数
+    int maxNumOfEnemyEnemyPrefab = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +27,14 @@ public class Spawn : MonoBehaviour
             Vector3 pos = new Vector3(x, y, 0);
             //プレハブを指定位置に生成
             Instantiate(EnemyPrefab, pos , Quaternion.identity);
+            //　この場所から出現する最大数を超えてたら何もしない
+            if (numberOfEnemyPrefab >= maxNumOfEnemyEnemyPrefab)
+            {
+                return;
+            }
         }
+
+        
+
     }
 }
