cbc13b84ef8344f4928f439649d2a15c89360c84
ma-si- <Kouma9140@gmail.com>
2023-07-06 18:51:16 +0900

    スポーン位置などの調整

--- bottisavivor/Assets/EnemyController.cs
+++ bottisavivor/Assets/EnemyController.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+public class enemyController : MonoBehaviour
 {
+    Vector3 Player;
+    GameObject Enemy;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //プレイヤーのオブジェクトを取得
+        Enemy = GameObject.Find("player");
+
+
+        Player = new Vector2();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //プレイヤーに向かうベクトルの作成
+        Player =Enemy.transform.position -this.transform.position;
+        Player = Player.normalized;
+        //自分自身をプレイヤーに向けて移動
+        this.transform.position +=  Player / 200;
     }
+
+    
+    
 }
--- bottisavivor/Assets/Spawn.cs
+++ bottisavivor/Assets/Spawn.cs
@@ -6,29 +6,40 @@ public class Spawn : MonoBehaviour
 {
     //プレハブ格納用
     public GameObject EnemyPrefab;
-       
+
 
     int count;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Time.frameCount % 1000 == 0)
+        count++;
+        if (count < 50)
         {
-            
-            float x = Random.Range(-5.0f, 5.0f);
-            float y = Random.Range(-5.0f, 5.0f);
-            Vector3 pos = new Vector3(x, y, 0);
-            //プレハブを指定位置に生成
-            Instantiate(EnemyPrefab, pos , Quaternion.identity);
-        }
-        
-        
 
+            float xleft = Random.Range(-202.0f,-158.0f);
+            float yleft = Random.Range(-70.0f, 70.0f);
+            Vector3 posleft = new Vector3(xleft, yleft, 0);
+            float xright = Random.Range(158.0f, 178.0f);
+            float yright = Random.Range(-70.0f, 70.0f);
+            Vector3 posright = new Vector3(xright, yright, 0);
+            float xtop = Random.Range(-150.0f, 190.0f);
+            float ytop = Random.Range(63.0f, 100.0f);
+            Vector3 postop = new Vector3(xtop, ytop, 0);
+            float xunder = Random.Range(-178.0f, 158.0f);
+            float yunder = Random.Range(-90.0f, -70.0f);
+            Vector3 posunder = new Vector3(xunder, yunder, 0);
+
+            //プレハブを指定位置に生成
+            Instantiate(EnemyPrefab, posleft, Quaternion.identity);
+            Instantiate(EnemyPrefab, posright, Quaternion.identity);
+            Instantiate(EnemyPrefab, postop, Quaternion.identity);
+            Instantiate(EnemyPrefab, posunder, Quaternion.identity);
+        }
+        if(count > 1000)
+        {
+            count = 0;
+        }
     }
 }
+
